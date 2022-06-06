b4c9ea93c9344f0aab291d1defc62b23a08576b3
Soufian <soufelhanafi@gmail.com>
2022-06-07 00:47:46 +0100

    fix issue with dashboard and translate

--- src/app/views/dashboard/dashboard0/DasboardGraphs.jsx
+++ src/app/views/dashboard/dashboard0/DasboardGraphs.jsx
@@ -407,7 +407,7 @@ const DashboardGraphs = () => {
 
             <div className="pb-2">
               <div className="table-responsive">
-                <table id="user_table" className="table  text-center">
+                <table id="user_table" className="table">
                   <thead>
                     <tr>
                       <th scope="col">#</th>
