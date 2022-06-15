abb1f8cecc7df9cdcb6b52f551c633c160b0dbe4
Soufian <soufelhanafi@gmail.com>
2022-06-15 18:21:17 +0100

    fix sorts

--- src/app/views/leadsNew/sections/TabsSection.jsx
+++ src/app/views/leadsNew/sections/TabsSection.jsx
@@ -10,21 +10,9 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
   const { t } = useTranslation();
   const profile = useSelector((state) => state.firebase.profile);
 
-  const [leads, setLeads] = useState(null);
   const [myLeads, setMyLeads] = useState([]);
-  const [prospect, setProspect] = useState(null);
   const [currentTab, setCurrentTab] = useState('Date');
-
-  useEffect(() => {
-    if (profile && data) {
-      const test = data?.filter(
-        (v) =>
-          !v?.broker || v?.broker[0]?.brokerId === profile.userId
-      );
-
-      setLeads(test);
-    }
-  }, [data, profile]);
+  const [ssort, setSort] = useState(null);
 
   const setUpdatedData = (id, broker=[]) => {
     const idx = myLeads.findIndex(item => item.id === id);
@@ -41,90 +29,28 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
     updateToBeContacted(item)
   }
 
-  useEffect(() => {
-    if (prospects) {
-      setProspect(prospects);
-    }
-  }, [prospects]);
-
   const options = [
-    { value: "price", label: t("Leads.19") },
-    { value: "price2", label: t("Leads.20") },
-    { value: "date", label: t("Leads.21") },
-    { value: "date2", label: t("Leads.22") },
+    { value: "desc_price", label: t("Leads.19") },
+    { value: "asc_price", label: t("Leads.20") },
+    { value: "asc_date", label: t("Leads.21") },
+    { value: "desc_date", label: t("Leads.22") },
     // { value: "stars", label: "Stars" },
   ];
 
-  function sort(valuePath, array) {
-    let path = valuePath.split(".");
-
-    return array.sort((a, b) => {
-      return getValue(b, path) - getValue(a, path);
-    });
-
-    function getValue(obj, path) {
-      path.forEach((path) => (obj = obj[path]));
-      return obj;
-    }
-  }
-
-  function sort2(valuePath, array) {
-    let path = valuePath.split(".");
-
-    return array.sort((a, b) => {
-      return getValue(a, path) - getValue(b, path);
-    });
-
-    function getValue(obj, path) {
-      path.forEach((path) => (obj = obj[path]));
-      return obj;
-    }
-  }
-
-  const onSorting = (value) => {
-    switch (value) {
-      case "price":
-        setLeads(sort("ziaEstimation.prediction", [...leads]));
-        setProspect(sort("ziaEstimation.prediction", [...prospect]));
-        break;
-
-      case "price2":
-        setLeads(sort2("ziaEstimation.prediction", [...leads]));
-        setProspect(sort2("ziaEstimation.prediction", [...prospect]));
-        break;
-
-      case "date":
-        const sortedLeads = [...leads].sort(function (a, b) {
-          return new Date(a.dateCreation) - new Date(b.dateCreation);
-        });
-        const sortedProspects = [...prospect].sort(function (a, b) {
-          return new Date(a.dateCreation) - new Date(b.dateCreation);
-        });
-        setLeads(sortedLeads);
-        setProspect(sortedProspects);
-        break;
-
-      case "date2":
-        const sortedLeadss = [...leads].sort(function (a, b) {
-          return new Date(b.dateCreation) - new Date(a.dateCreation);
-        });
-        const sortedProspectss = [...prospect].sort(function (a, b) {
-          return new Date(b.dateCreation) - new Date(a.dateCreation);
-        });
-        setLeads(sortedLeadss);
-        setProspect(sortedProspectss);
-        break;
-
-      case "stars":
-        console.log("Oranges are $0.59 a pound.");
-        break;
-
+  const sortedData = (data = [], value) => {
+    switch(value){
+      case "asc_price":
+        return data.sort((a,b) => a.ziaEstimation.prediction > b.ziaEstimation.prediction ? 1:-1);
+      case "desc_price":
+        return data.sort((a,b) => a.ziaEstimation.prediction > b.ziaEstimation.prediction ? -1:1);
+      case "desc_date":
+        return data.sort((a,b) => a.dateCreation > b.dateCreation ? -1:1);
+      case "asc_date":
+        return data.sort((a,b) => a.dateCreation > b.dateCreation ? 1:-1);
       default:
-        console.log(`Sorry, we are out of ${value}.`);
+        return data;
     }
-  };
-
-  console.log(prospect)
+  }
 
   return (
     <Tab.Container id="left-tabs-example" defaultActiveKey="Date" onSelect={key => setCurrentTab(key)}>
@@ -138,7 +64,7 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
               <Select
                 placeholder={t("Leads.76")}
                 options={options}
-                onChange={(e) => onSorting(e.value)}
+                onChange={(e) => setSort(e.value)}
               />
             </li>
             <li className="ml-md-4 flex-fill">
@@ -167,7 +93,7 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
             <Tab.Pane eventKey="Price">
               {currentTab === 'Price' &&
                   <ul className="nav flex-column gy-2">
-                    {tobecontacted?.map((item) => (
+                    {sortedData(tobecontacted, ssort).map((item) => (
                         <li key={item.id}>
                           <NewLeadCard data={item} onClick={onClick} reports={reports} setUpdatedData={setUpdatedData} updateMyLeads={updateMyLeads} />
                         </li>
@@ -179,7 +105,7 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
               {
                 currentTab === 'Date' &&
                   <ul className="nav flex-column gy-2">
-                    {prospects?.map((item) => (
+                    {sortedData(prospects, ssort)?.map((item) => (
                         <li>
                           <NewLeadCard
                               data={item}
@@ -198,7 +124,7 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
               {
                 currentTab === 'MyLeads' &&
                   <ul className="nav flex-column gy-2">
-                    {myLeads.map((item) => (
+                    {sortedData(myLeads, ssort).map((item) => (
                         <li>
                           <NewLeadCard data={item} onClick={onClick} reports={reports} setUpdatedData={setUpdatedData} />
                         </li>
@@ -209,7 +135,7 @@ const TabsSection = ({ data, onClick, prospects, reports = [], tobecontacted=[],
             <Tab.Pane className="h-100" eventKey="Bookmarks">
               {currentTab === 'Bookmarks' &&
                   <ul className="nav flex-column gy-2">
-                    {profile?.bookmarks?.map((item) => (
+                    {sortedData(profile?.bookmarks, ssort).map((item) => (
                         <li>
                           <NewLeadCard
                               data={item}
